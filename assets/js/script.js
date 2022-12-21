8c9ebeae640fbdb4f0235a2a972ae34506663ab5
Kishan <kishan.chandravadia@zealousweb.com>
2022-12-21 20:02:52 +0530

    new

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,7 +15,37 @@ navItems.forEach(element => {
 
 let burger = document.querySelector(".nav-btn");
 let nav=document.querySelector("nav")
+let menu = document.querySelector('.menu')
 burger.addEventListener("click",()=>{
     nav.classList.toggle("open")
     burger.classList.toggle("open")
+})
+
+
+let accordian = [...document.getElementsByClassName("accordion-card-title")];
+
+accordian.forEach((element,index)=>{
+    let arrow = element.lastElementChild;
+    let content = element.nextElementSibling;
+    let flag = 0
+    arrow.addEventListener("click",()=>{
+        for(let i=0;i<5;i++){
+            if(index!=i){
+                console.log(index,i)
+                accordian[i].nextElementSibling.style.maxHeight=null;
+            }
+        }
+        if(flag==0){
+            content.style.maxHeight=content.scrollHeight+"px";
+            flag=1;
+        }
+        else{
+            content.style.maxHeight=null;
+            flag=0;
+        }
+        content.classList.toggle("active")
+        console.log(content.classList)
+        
+    })
+
 })
